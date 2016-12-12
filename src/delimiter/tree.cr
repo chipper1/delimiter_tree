4ffe77a0bd6ba44b4de2dd00796dc1f11d736b08
Dru Jensen <drujensen@gmail.com>
2016-12-11 18:57:21 -0800

    refactor logic for find

--- src/delimiter/tree.cr
+++ src/delimiter/tree.cr
@@ -26,35 +26,29 @@ module Delimiter
       result = Result(T).new
 
       pos = @root
-      last_pos = nil
+      last_pos = pos
+
       parts = path.split(@delimiter)
       parts.each do |part|
-        @found = false
-
-        # check if this pos has any wildcards and add them.  
-        # only add this if its not already been added last_pos.
-        if pos.children.has_key?("*") && pos != last_pos
+        if pos.children.has_key?("*") 
           pos.children["*"].payload.each {|p| result.payload << p}
-          @found = !result.payload.empty?
-          last_pos = pos
         end
         
         if pos.children.has_key? part
-          @found = true
           pos = pos.children[part]
         else
-          #might be a better way to do this
           pos.children.each_key do |key|
             if key.starts_with? ":"
               result.params[key.sub(":", "")] = part
               pos = pos.children[key]
-              @found = true
             end
           end
         end
+
+        pos == last_pos ?  break :(last_pos = pos)
       end
-      result.found = (@found == true)
       pos.payload.each {|p| result.payload << p}
+      result.found = !result.payload.empty?
       result
     end
   end
